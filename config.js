fc318692a6729b45e259ade4f529f5d80f77cb99
Hrafnir <eirik.gaarde@gmail.com>
2025-12-18 10:29:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,21 @@
-/* Version: #1 */
-// LIM INN DIN FIREBASE CONFIG HER FRA GOOGLE CONSOLE
-// Det skal se ca slik ut (men med dine tall og bokstaver):
+/* Version: #2 */
 
+// Din konfigurasjon (kopiert fra Google, men tilpasset vår app)
 const firebaseConfig = {
-  apiKey: "AIzaSyD......",
-  authDomain: "ditt-prosjekt.firebaseapp.com",
-  databaseURL: "https://ditt-prosjekt-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "ditt-prosjekt",
-  storageBucket: "ditt-prosjekt.appspot.com",
-  messagingSenderId: "123456...",
-  appId: "1:123456..."
+  apiKey: "AIzaSyAgLx5nzkS_kzQIM5sNH17oOx4m5iICtRA",
+  authDomain: "mentimeter-b5692.firebaseapp.com",
+  databaseURL: "https://mentimeter-b5692-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "mentimeter-b5692",
+  storageBucket: "mentimeter-b5692.firebasestorage.app",
+  messagingSenderId: "717520203326",
+  appId: "1:717520203326:web:74d0095edf3a472dd46b6f",
+  measurementId: "G-MVFE5KGTF9"
 };
 
-// Initialiser Firebase (Ikke endre dette)
-firebase.initializeApp(firebaseConfig);
+// Initialiser Firebase (Sjekker om den allerede kjører for å unngå feil)
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+// Gjør databasen klar til bruk for host.html og client.html
 const db = firebase.database();
